6ebee2f55ec7d28c9274a927ae777d0e3a914780
Diogo Neves <diogo.neves@gmail.com>
2015-05-06 20:52:54 +0100

    Created package tool

---
+++ package.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+if [ -d ./classes ]
+then
+    echo "Removing class files"
+    rm -rf classes
+fi
+
+if [ -f rsbot.jar ]
+then
+    echo "Removing package"
+    rm rsbot.jar
+fi
+
+echo "Creating classes directory"
+mkdir classes
+
+echo "Compiling"
+javac -Xlint:deprecation -Xlint:unchecked -d classes RSBot.java
+
+echo "Creating manifest"
+echo "Main-Class: RSBot" > classes/manifest.txt
+echo "" >> classes/manifest.txt
+
+echo "Packaging"
+cd classes
+
+jar -cvmf manifest.txt ../rsbot.jar *
+
+cd -
